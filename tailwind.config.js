0ba33b0b455a0ead32a17456eccdfa5e16b73751
joacotournier <87491798+joacotournier@users.noreply.github.com>
2023-06-18 12:00:35 -0300

    Rough structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "main-pattern": "url('/main-pattern.jpg')",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
