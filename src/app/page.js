7ddeebbaa07398e7ce3ea9d5429c0ff16205138f
joacotournier <87491798+joacotournier@users.noreply.github.com>
2023-06-25 16:16:25 -0300

    further structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,107 +1,72 @@
 "use client";
-
 import Image from "next/image";
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import useSWR from "swr";
 
-export default function Example() {
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Page() {
+  const { data, error } = useSWR("/sections.json", fetcher);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
     <div className="bg-main-pattern bg-cover h-screen">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          className="flex items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <Image
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt=""
-                width={128}
-                height={60}
-              />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
-        </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
-          <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <Image
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                  alt=""
-                  width={128}
-                  height={60}
-                />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-400"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/25">
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                  >
-                    Log in
-                  </a>
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
+        {/* Header code here */}
       </header>
 
       <div className="relative isolate pt-14">
-        <div className="py-24 sm:py-32 lg:pb-40">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl text-center">
-              <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                Data to enrich your online business
-              </h2>
-              <img
-                src="/grabadora.png"
-                alt="grabadora"
-                className="drop-shadow-xl"
-              />
+        {data.map((section) => (
+          <div className="py-24 sm:py-32 lg:pb-40" key={section.id}>
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl text-center">
+                {section.titulo && (
+                  <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                    {section.titulo}
+                  </h2>
+                )}
+                {section.sobreTitulo && (
+                  <h3 className="text-3xl font-semibold text-white sm:text-4xl">
+                    {section.sobreTitulo}
+                  </h3>
+                )}
+                {section.periodo && (
+                  <p className="text-xl text-white">{section.periodo}</p>
+                )}
+                {section.description && (
+                  <p className="text-lg text-white mt-2">
+                    {section.description}
+                  </p>
+                )}
+                {section.imagen && (
+                  <img
+                    src={section.imagen}
+                    alt={section.caption || "Section image"}
+                    className="drop-shadow-xl"
+                  />
+                )}
+                {section.caption && (
+                  <p className="text-sm text-white mt-2">{section.caption}</p>
+                )}
+                {section.quote && (
+                  <blockquote className="text-lg font-medium italic text-white mt-4">
+                    "{section.quote}"
+                  </blockquote>
+                )}
+                {section.imagenQuote && (
+                  <figcaption className="text-sm text-white mt-2">
+                    {section.imagenQuote}
+                  </figcaption>
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
