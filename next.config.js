1e1f8981ea2ae67705d99a8c2969b7b90aa2af76
joacotournier <87491798+joacotournier@users.noreply.github.com>
2023-06-26 21:11:04 -0300

    config rollback

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === "production" ? "/lauro-ayestaran" : "",
   output: "export",
   images: {
     loader: "custom",
