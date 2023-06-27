9b36bf475eefbeef48573387a2993a9763434f6f
joacotournier <87491798+joacotournier@users.noreply.github.com>
2023-06-26 21:01:28 -0300

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import { useState } from "react";
 import useSWR from "swr";
 import Section from "./components/section";
 import LoadingScreen from "./components/LoadingScreen";
+import { useRouter } from "next/router";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Page() {
-  const { data, error } = useSWR("/sections.json", fetcher);
+  const router = useRouter();
+  const { data, error } = useSWR(`${router.basePath}/sections.json`, fetcher);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const showLoading = false; // REMEMBER TO CHANGE THIS WHEN DEPLOYING
