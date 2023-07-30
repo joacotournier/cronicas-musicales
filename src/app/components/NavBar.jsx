2aa054038925060535754ee7f116895100e66a3e
joacotournier <87491798+joacotournier@users.noreply.github.com>
2023-07-30 18:43:14 -0300

    menu

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,6 +1,31 @@
 import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
+
+const sections = [
+  {
+    title: "Etapa I",
+    href: "#i",
+    date: "1942 - 1950",
+    image: "/paysandu-1.jpg",
+  },
+  {
+    title: "Etapa II",
+    href: "#ii",
+    date: "1951 - 1960",
+    image: "/paysandu-1.jpg",
+  },
+  {
+    title: "Etapa III",
+    href: "#iii",
+    date: "1961 - 1970",
+    image: "/paysandu-1.jpg",
+  },
+  {
+    title: "Legado",
+    href: "#legado",
+    date: "1971 - 1980",
+    image: "/paysandu-1.jpg",
+  },
+];
 
 function NavBar({ currentSection }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,19 +47,33 @@ function NavBar({ currentSection }) {
         <img src="/menu.svg" alt="Menu Icon" width={12} height={12} />
       </div>
       {isMenuOpen && (
-        <div className="fixed top-0 left-0 w-screen h-screen bg-black text-center text-white flex flex-col justify-center items-center">
-          <a href="#i" onClick={closeMenu}>
-            Etapa I
-          </a>
-          <a href="#ii" onClick={closeMenu}>
-            Etapa II
-          </a>
-          <a href="#iii" onClick={closeMenu}>
-            Etapa III
-          </a>
-          <a href="#legado" onClick={closeMenu}>
-            Legado
-          </a>
+        <div
+          className="fixed top-0 left-0 w-screen h-screen bg-center bg-cover text-center text-white flex items-stretch"
+          style={{ backgroundImage: `url(/main-pattern.jpg)` }}
+        >
+          {sections.map((section, index) => (
+            <a
+              href={section.href}
+              onClick={closeMenu}
+              className={`relative flex-1 h-full flex flex-col justify-center items-center ${
+                index === sections.length - 1
+                  ? ""
+                  : "border-r border-gray-500 border-opacity-15"
+              }`}
+              key={section.title}
+            >
+              <div
+                className="absolute z-10 inset-0 bg-center bg-cover opacity-0 hover:opacity-20 transition-opacity duration-200"
+                style={{ backgroundImage: `url(${section.image})` }}
+              ></div>
+              <div className="font-bold text-3xl text-white w-full h-full flex flex-col justify-center items-center">
+                {section.title}
+                <span className="text-xs text-white opacity-50">
+                  {section.date}
+                </span>
+              </div>
+            </a>
+          ))}
         </div>
       )}
     </nav>
