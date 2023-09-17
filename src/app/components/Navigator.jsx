d4a72edd83b109e45a6813170c5c8a559c201b9e
joacotournier <87491798+joacotournier@users.noreply.github.com>
2023-09-17 14:22:20 -0300

    update navigator

--- src/app/components/Navigator.jsx
+++ src/app/components/Navigator.jsx
@@ -1,24 +1,58 @@
 import React from "react";
+import { useRef, useEffect } from "react";
 
 function Navigator({ currentSection, sections }) {
-  const indexOfCurrent = sections.findIndex((s) => s.etapa === currentSection);
-  const displayedSections = sections.slice(
-    Math.max(0, indexOfCurrent - 2),
-    indexOfCurrent + 3
-  );
+  const navigatorRef = useRef();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (navigatorRef.current) {
+        const offsetTop = navigatorRef.current.getBoundingClientRect().top;
+        const halfHeight = navigatorRef.current.clientHeight / 2;
+
+        sections.forEach((section) => {
+          const el = document.getElementById(section.etapa.toLowerCase());
+          if (el) {
+            const elTop = el.getBoundingClientRect().top;
+            if (
+              elTop <= offsetTop + halfHeight &&
+              elTop + el.clientHeight > offsetTop + halfHeight
+            ) {
+              if (currentSection !== section.etapa) {
+                currentSection = section.etapa;
+              }
+            }
+          }
+        });
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [currentSection, sections]);
+
+  const scrollToSection = (id) => {
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
-    <div className="fixed z-10 right-0 top-1/2 transform -translate-y-1/2 space-y-4">
-      {displayedSections.map((section, index) => (
+    <div
+      ref={navigatorRef}
+      className="fixed right-0 top-1/2 transform -translate-y-1/2 z-50"
+    >
+      {sections.map((section, index) => (
         <div
-          key={section.id}
-          className={`transition-opacity duration-300 text-center ${
-            section.etapa === currentSection
-              ? "opacity-100 text-2xl"
-              : "opacity-50 text-lg"
+          key={index}
+          onClick={() => scrollToSection(section.etapa.toLowerCase())}
+          className={`cursor-pointer mb-2 p-2 pr-10 text-right opacity-50 hover:opacity-75 ${
+            currentSection === section.etapa ? "text-xl opacity-100" : ""
           }`}
         >
-          {section.etapa}
+          {section.tituloNavegador}
         </div>
       ))}
     </div>
