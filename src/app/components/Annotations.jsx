9f627ec2e3679afe6800070753eec6caa1aecd1e
joacotournier <87491798+joacotournier@users.noreply.github.com>
2023-10-29 19:19:17 -0300

    fixes

--- src/app/components/Annotations.jsx
+++ src/app/components/Annotations.jsx
@@ -49,16 +49,38 @@ function Annotations({ isOpen, annotationId, onClose }) {
   return (
     <div
       ref={annotationRef}
-      className="fixed z-[100] top-0 right-0 h-full bg-slate-200 text-black p-10 overflow-y-auto"
+      className="fixed z-[100] top-0 right-0 h-full bg-slate-200 text-black p-10 pr-20 overflow-y-auto"
     >
       <button
         onClick={onClose}
-        className="absolute top-5 right-5 text-xl font-bold hover:text-gray-600 transition"
+        className="absolute top-5 right-5 text-2xl font-bold hover:text-gray-600 transition"
       >
         ×
       </button>
-      <h1 className="text-3xl font-semibold mb-4">{annotation.title}</h1>
-      <p className="text-base">{annotation.content}</p>
+      <h1 className="text-3xl font-semibold mb-4 max-w-sm">
+        {annotation.title}
+      </h1>
+      {Array.isArray(annotation.content) ? (
+        annotation.content.map((item, index) => {
+          if (typeof item === "string") {
+            return (
+              <p key={index} className="text-base max-w-sm">
+                {item}
+              </p>
+            );
+          }
+          if (item.type === "image") {
+            return (
+              <figure key={index} className="my-4">
+                <img src={item.src} alt={item.caption} className="max-w-sm" />
+                <figcaption className="text-sm mt-2">{item.caption}</figcaption>
+              </figure>
+            );
+          }
+        })
+      ) : (
+        <p className="text-base">{annotation.content}</p>
+      )}
     </div>
   );
 }
